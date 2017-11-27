c6e6266ac701651d586acdf1e36e9e6cab951012
Jay McClow <jay.mcclow1@ge.com>
2017-11-27 09:28:13 -0800

    Need a kitchen yaml

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['chef_node']['network']['public'] = '107.184.96.158'
 default['chef_node']['network']['bind'] = '10.0.2.15'
-default['chef_node']['keys']['priv'] = '88TkmS6pR2umbXa5JSiNgJiua2BYdAo6vRug5SMMnyi8eFxiLSU'
+default['chef_node']['keys']['priv'] = ''
 default['chef_node']['coin'] = 'pivx'
 default['chef_node']['tarball'] = 'pivx-2.2.1-x86_64-linux-gnu.tar.gz'
 default['chef_node']['package'] = 'pivx-2.2.1'
